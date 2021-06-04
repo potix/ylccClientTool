ab6b1cdadd37571d80f568dcb2dad250bfe4d65f
poti <poti.dog@gmail.com>
2021-06-04 19:08:36 +0900

    update

--- ylccClientTool/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ ylccClientTool/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -525,7 +525,7 @@ namespace ylccClientTool {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ylccClientTool;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ylccClientTool;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
